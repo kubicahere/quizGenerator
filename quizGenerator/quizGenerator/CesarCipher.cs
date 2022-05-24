7edbcc3b338153f1e330d4ccb25448b1f49ae0bb
Jakub Nowak <39097304+kubicahere@users.noreply.github.com>
2022-05-24 13:37:43 +0200

    Add files via upload

---
+++ quizGenerator/quizGenerator/CesarCipher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quizGenerator
+{
+    public static class CesarCipher
+    {
+        static private char coding(char ch, int key)
+        {
+            if (!char.IsLetter(ch))
+            {
+
+                return ch;
+            }
+
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + key) - d) % 26) + d);
+        }
+        public static string Caesar_coding(string text, int key)
+        {
+            string output = string.Empty;
+
+            foreach (char ch in text)
+                output += coding(ch, key);
+
+            return output;
+        }
+
+        public static string Caesar_decoding(string text, int key)
+        {
+            return Caesar_coding(text, 26 - key);
+        }
+    }
+}
